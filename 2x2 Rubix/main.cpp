a98ca5b8ffd9f5fc0ec97558a14714be542a11d7
juriyuzu <juliusantonaquino.joules@gmail.com>
2023-09-18 20:46:20 +0800

    redid the entire rubix array mechanism to a way easier one

--- 2x2 Rubix/main.cpp
+++ 2x2 Rubix/main.cpp
@@ -6,16 +6,21 @@ void display();
 void pass();
 
 int face = 2;
-const int F = 6, Y = 2, X = 2;
-char rubix[F][Y][X] = {
-    {{'A', 'A'}, {'a', 'A'}},
-    {{'B', 'B'}, {'B', 'B'}},
-    {{'c', 'C'}, {'C', 'C'}},
-    {{'D', 'D'}, {'D', 'D'}},
-    {{'E', 'E'}, {'E', 'E'}},
-    {{'F', 'F'}, {'F', 'F'}}
+const int X = 11;
+char rubix[X][X] = {
+    {'0', '0', '0', 'A', 'A', '0', '0', '0', '0', '0', '0'}, 
+    {'0', '0', '0', 'A', 'A', '0', '0', '0', '0', '0', '0'}, 
+    {'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'}, 
+    {'B', 'B', '0', 'C', 'C', '0', 'D', 'D', '0', 'F', 'F'}, 
+    {'B', 'B', '0', 'C', 'C', '0', 'D', 'D', '0', 'F', 'F'}, 
+    {'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'}, 
+    {'0', '0', '0', 'E', 'E', '0', '0', '0', '0', '0', '0'}, 
+    {'0', '0', '0', 'E', 'E', '0', '0', '0', '0', '0', '0'}, 
+    {'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'}, 
+    {'0', '0', '0', 'F', 'F', '0', '0', '0', '0', '0', '0'}, 
+    {'0', '0', '0', 'F', 'F', '0', '0', '0', '0', '0', '0'}, 
 };
-char rubix2[F][Y][X];
+char rubix2[X][X];
 
 int main() {
     play();
@@ -27,8 +32,6 @@ void play() {
     char a;
     int ccw;
     bool d;
-    int iFR[5][2] = {{0, 0}, {1, 0}, {1, 1}, {0, 1}, {0, 0}};
-    int oFR[5][3][2] = {{{1}, {0, 0}, {1, 0}}, {{-2}, {1, 0}, {1, 1}}, {{-1}, {1, 1}, {0, 1}}, {{2}, {0, 1}, {0, 0}}, {{1}, {0, 0}, {1, 0}}};
 
     display();
 
@@ -36,9 +39,8 @@ void play() {
         pass();
         ccw = 0;
         d = true;
-        cout << "> ";
+        cout << endl << "> ";
         cin >> a;
-        cout << endl << a << endl;
 
         if (a == '-') {
             ccw = 1;
@@ -47,11 +49,6 @@ void play() {
 
         switch (a) {
             case 'F':
-                for (int i = 0 + ccw; i < 4 + ccw; i++) {
-                    rubix[face][iFR[i][0]][iFR[i][1]] = rubix2[face][iFR[i + (ccw ? -1 : 1)][0]][iFR[i + (ccw ? -1 : 1)][1]];
-                    rubix[face + oFR[i][0][0]][oFR[i][1][0]][oFR[i][1][1]] = rubix2[face + oFR[i + (ccw ? -1 : 1)][0][0]][oFR[i + (ccw ? -1 : 1)][1][0]][oFR[i + (ccw ? -1 : 1)][1][1]];
-                    rubix[face + oFR[i][0][0]][oFR[i][2][0]][oFR[i][2][1]] = rubix2[face + oFR[i + (ccw ? -1 : 1)][0][0]][oFR[i + (ccw ? -1 : 1)][2][0]][oFR[i + (ccw ? -1 : 1)][2][1]];
-                }
                 break;
             case 'B':
                 break;
@@ -64,7 +61,6 @@ void play() {
             case 'D':
                 break;
             case 'x':
-                
                 break;
             case 'y':
                 break;
@@ -79,53 +75,17 @@ void play() {
 }
 
 void display() {
-    // for (int i = 0; i < 3; i++) {
-    //     for (int j = 0; j < 6; j++) {
-    //         for (int k = 0; k < 3; k++) 
-    //             cout << rubix[j][i][k] << " ";
-    //         cout << " | ";
-    //     }
-    //     cout << endl;
-    // }
     cout << endl;
-    for (int i = 0; i < Y; i++) {
-        cout  << "      ";
+    for (int i = 0; i < X; i++) {
         for (int j = 0; j < X; j++) 
-            cout << rubix[0][i][j] << ' ';
+            cout << (rubix[i][j] != '0' ? rubix[i][j] : ' ') << ' ';
         cout << endl;
     }
-    cout << endl;
-    for (int k = 0; k < Y; k++) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < X; j++) 
-                cout << rubix[i + 1][k][j] << ' ';
-            cout << "  ";
-        }
-        cout << endl;
-    }
-    cout << endl;
-    for (int i = 0; i < Y; i++) {
-        cout  << "      ";
-        for (int j = 0; j < X; j++) 
-            cout << rubix[4][i][j] << ' ';
-        cout << endl;
-    }
-    cout << endl;
-    for (int i = 0; i < Y; i++) {
-        cout  << "      ";
-        for (int j = 0; j < X; j++) 
-            cout << rubix[5][i][j] << ' ';
-        cout << endl;
-    }
-    cout << endl;
 }
 
 void pass() {
-    for (int i = 0; i < F; i++) {
-        for (int j = 0; j < Y; j++) {
-            for (int k = 0; k < X; k++) {
-                rubix2[i][j][k] = rubix[i][j][k];
-            }
-        }
+    for (int i = 0; i < X; i++) {
+        for (int j = 0; j < X; j++) 
+            rubix2[i][j] = rubix[i][j];
     }
 }
